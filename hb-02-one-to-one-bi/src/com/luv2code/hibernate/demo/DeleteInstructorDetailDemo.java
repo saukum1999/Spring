2a987e3876fb28f60b89f521a21733a3d1941eb4
sauku <sauku@MSI>
2022-06-17 00:06:03 +0530

    Hibernate One-To-One mapping Bi-Directional

---
+++ hb-02-one-to-one-bi/src/com/luv2code/hibernate/demo/DeleteInstructorDetailDemo.java
@@ -0,0 +1,58 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+//import com.luv2code.hibernate.demo.entity.Student;
+
+public class DeleteInstructorDetailDemo {
+
+	public static void main(String[] args) {
+		
+		SessionFactory factory = new Configuration()
+									.configure("hibernate.cfg.xml")
+									.addAnnotatedClass(Instructor.class)
+									.addAnnotatedClass(InstructorDetail.class)
+									.buildSessionFactory();
+		// TODO Auto-generated method stub
+
+		Session session = factory.getCurrentSession();
+		
+		try {
+			
+			session.beginTransaction();
+			
+			int id = 6;
+			InstructorDetail tempInstructorDetail = session.get(InstructorDetail.class,id);
+			
+			System.out.println("Found Instructor Detail: "+tempInstructorDetail);
+			
+			System.out.println("Print associate Instructor: "+ tempInstructorDetail.getInstructor());
+			
+			// remove associated object link
+			
+			tempInstructorDetail.getInstructor().setInstructorDetail(null);
+			if(tempInstructorDetail != null) {
+				System.out.println("Deleting Instructor: " + tempInstructorDetail);
+				session.delete(tempInstructorDetail);
+			}
+					
+			session.getTransaction().commit();
+			System.out.println("Done!");
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			// TODO: handle exception
+		}
+	
+		finally{
+			session.close();
+		factory.close();
+			
+		}
+	}
+
+}
---
+++ hb-02-one-to-one-bi/src/com/luv2code/jdbc/TestJdbc.java
@@ -0,0 +1,28 @@
+package com.luv2code.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?useSSL=false&serverTimezone=UTC";
+		String user = "hbstudent";
+		String pass = "hbstudent";
+
+		try {
+			System.out.println("Connecting to Database : " + jdbcUrl);
+
+//			Connection con = DriverManager.getConnection(jdbcUrl,user,pass);
+			
+			System.out.println("Connection Successfull !!!!");
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			
+		}
+	}
+
+}
