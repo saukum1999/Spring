a4f6c772f7df740f946271fc0539cd99b1ed5d6a
saukum1999 <saukum1999@gmail.com>
2022-07-05 00:00:31 +0530

    Spring Security - Custom Login Form.

--- spring-security-demo-03-custom-login-form/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ spring-security-demo-03-custom-login-form/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -13,6 +13,16 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.anyRequest().authenticated()
+			.and()
+			.formLogin()
+			.loginPage("/showLoginPage")
+			.loginProcessingUrl("/authenticateTheUser")
+			.permitAll();
+	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
