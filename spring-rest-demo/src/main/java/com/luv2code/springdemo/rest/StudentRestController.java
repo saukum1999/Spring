c626758e5d93b2e54a22e4cc1222d37298fef43e
saukum1999 <saukum1999@gmail.com>
2022-07-12 22:44:21 +0530

    Spring REST - Exception Handling - Generic Exception Handling.

--- spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,8 +39,38 @@ public class StudentRestController {
 	
 	@GetMapping("/students/{studentId}")
 	public Student getStudent(@PathVariable int studentId) {
+		
+//		check the studentId against the students list
+		if((studentId >= theStudents.size()) || (studentId < 0)) {
+			throw new StudentNotFoundException("Student Id not found - "+studentId);
+		}
 				
 		return theStudents.get(studentId);
 		
+	}
+	
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResposne> handleException (StudentNotFoundException exc){
+		
+		StudentErrorResposne error = new StudentErrorResposne();
+		
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+		
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+	}
+	
+//	Generic Exception Handler
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResposne> handleException (Exception exc){
+		
+		StudentErrorResposne error = new StudentErrorResposne();
+		
+		error.setMessage(exc.getMessage());
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		error.setTimeStamp(System.currentTimeMillis());
+		
+		return new ResponseEntity<> (error, HttpStatus.BAD_REQUEST);
 	}
 }
