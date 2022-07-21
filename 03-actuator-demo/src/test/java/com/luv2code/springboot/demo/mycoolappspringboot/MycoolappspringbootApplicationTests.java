dcd1f1d6b638f4bf17fe9d13877877d308207718
saukum1999 <saukum1999@gmail.com>
2022-07-21 19:32:28 +0530

    Spring Boot App with Actuators.

---
+++ 03-actuator-demo/src/test/java/com/luv2code/springboot/demo/mycoolappspringboot/MycoolappspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.demo.mycoolappspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
